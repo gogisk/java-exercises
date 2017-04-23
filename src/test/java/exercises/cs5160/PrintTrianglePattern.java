445bf8cdef1231fd59b5281a806fc094401ce44c
gogisk <sivakumarnganesan@outlook.com>
2017-04-22 23:16:30 -0400

    04222017

---
+++ src/test/java/exercises/cs5160/PrintTrianglePattern.java
@@ -0,0 +1,16 @@
+package exercises.cs5160;
+
+/**
+ * Created by gsk
+ * on 4/22/2017.
+ */
+public class PrintTrianglePattern {
+    /**
+     * Write a function that prints a triangle shaped pattern using space and asterisk characters.
+     * Your function should accept one parameter that determines the number of rows of asterisks to display.
+     * This value will always be a positive integer greater than zero (e.g. 1, 2, 3, …).
+     */
+    public static void main(String[] args) {
+
+    }
+}
